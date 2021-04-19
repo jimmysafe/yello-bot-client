0e173d66f0503fc41af35710265f10dd4f7f6657
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2021-04-19 22:18:26 +0200

    add new paypal button

--- components/Paypal.tsx
+++ components/Paypal.tsx
@@ -1,74 +1,54 @@
-import { useRef, useEffect, FC, useState } from 'react'
-import Success from './checkout/Success'
-import { useGuildUpgradeMutation } from '../graphql/generated'
-import Loading from '../components/Loading'
+import { PayPalButton } from 'react-paypal-button-v2';
+import { FC, useState } from 'react';
+import Success from './checkout/Success';
+import { useGuildUpgradeMutation } from '../graphql/generated';
+import Loading from '../components/Loading';
 
 type Props = {
-  close: () => void,
-  guild_id: string
-}
+	close: () => void;
+	guild_id: string;
+};
 
 declare global {
-  interface Window { paypal: any; }
+	interface Window {
+		paypal: any;
+	}
 }
-
 
 const Paypal: FC<Props> = ({ close, guild_id }) => {
+	const [upgradeGuild] = useGuildUpgradeMutation();
 
-  const [upgradeGuild] = useGuildUpgradeMutation()
+	const [success, setSuccess] = useState<boolean>(false);
+	const [error, setError] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
 
-  const [success, setSuccess] = useState<boolean>(false)
-  const [error, setError] = useState<string>('')
-  const [loading, setLoading] = useState<boolean>(false)
+	return (
+		<>
+			{success && <Success close={close} />}
+			{loading && <Loading />}
+			<PayPalButton
+				amount='0.01'
+				shippingPreference='NO_SHIPPING'
+				onSuccess={async () => {
+					setLoading(true);
+					setError('');
+					await upgradeGuild({ variables: { guild_id } });
+					setLoading(false);
+					setSuccess(true);
+				}}
+				onError={(err) => {
+					setError(err);
+					setLoading(false);
+					console.error(err);
+				}}
+				options={{
+					clientId: process.env.PAYPAL_CLIENT_ID,
+					currency: 'EUR',
+				}}
+			/>
+			{error && <p className='mt-3 text-white text-center'>{error}</p>}
+		</>
+	);
+};
 
-  const paypalRef: any = useRef();
-
-  useEffect(() => {
-      window.paypal
-      .Buttons({
-        createOrder: (_, actions:any) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: "Yello Premium - Lifetime Plan",
-                amount: {
-                  currency_code: "EUR",
-                  value: 4.99,
-                },
-              },
-            ],
-          });
-        },
-        onApprove: async (_, actions: any) => {
-          setLoading(true)
-          await actions.order.capture();
-          setError('')
-          await upgradeGuild({ variables: { guild_id } })
-          setLoading(false)
-          setSuccess(true)
-        },
-        onError: (err) => {
-          setError(err)
-          setLoading(false)
-          console.error(err);
-        },
-      })
-      .render(paypalRef.current);
-  }, [])
-  
-  return (
-      <>
-      {success && <Success close={close}/>}
-      {loading && <Loading /> }
-      <div ref={paypalRef} />
-      {error && (
-        <p className="mt-3 text-white text-center">
-          {error}
-        </p>
-      )}
-      </>
-  )
-}
-
-export default Paypal
+export default Paypal;
